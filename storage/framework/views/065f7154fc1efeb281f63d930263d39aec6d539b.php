4add03c7869ef474c3a471ffc83d0883dc35a85a
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2022-03-02 10:46:09 +0700

    first-commit

---
+++ storage/framework/views/065f7154fc1efeb281f63d930263d39aec6d539b.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+        Đây là trang chủ
+        <a href="/vutao">Home</a>
+    </body>
+</html>
+<?php /**PATH D:\VuNgu\resources\views/trangchu.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b142fb7fc96078247959f49a47b9eea89a73d4b0.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+        Đây là giới thiệu.
+        <a href="/vutao">Home</a>
+    </body>
+</html>
+<?php /**PATH D:\VuNgu\resources\views/gioithieu.blade.php ENDPATH**/ ?>
